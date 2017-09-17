5796ece09958d596fe91c5d7a378b1acf4bf8075
Jack Peterson <jack@tinybike.net>
2017-09-17 12:01:15 -0700

    Added mp4-to-mp3 conversion script

---
+++ convert-mp4-to-mp3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Note: requires ffmpeg
+# https://askubuntu.com/a/174300
+
+set -e
+
+for f in $1/*.mp4
+do
+    name=`echo "$f" | sed -e "s/.mp4$//g"`
+    ffmpeg -i "$f" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "$name.mp3"
+done
